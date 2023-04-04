25ace4856faa883760e0323ca580a8d8e4640851
qijunyin <june@qijunyindeMacBook-Pro.local>
2023-04-04 21:29:29 +0800

    增加测试

--- test/NFTSwapTest.ts
+++ test/NFTSwapTest.ts
@@ -3,7 +3,7 @@ import { expect } from "chai";
 import chai from "chai";
 import { solidity } from "ethereum-waffle";
 import { beforeEach } from "mocha";
-import { Contract } from "ethers";
+import { BigNumber, Contract } from "ethers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 chai.use(solidity);
@@ -57,12 +57,47 @@ describe("初始化ApeNft并mint", () => {
         //     nFTSwapContract,
         //     "List"
         // ).withArgs(owner.address, wtfApeContract.address, 2, 100);
-        
-        // const orderNft = await nFTSwapContract.nftList[wtfApeContract.address][2];
-        console.log(await nFTSwapContract.nftList(wtfApeContract.address, 2), "order信息");
-        // expect(orderNft.owner).to.equal(await wtfApeContract.address);
-        // expect(await wtfApeContract.getApproved(1)).to.equal(await nFTSwapContract.address);
+        const orderNft = await nFTSwapContract.nftList(wtfApeContract.address, 2);
+        expect(orderNft.owner).to.equal(owner.address);
     })
 
+    it("更新nft价格", async() => {
+        // wtfApeContract 合约mint
+        await wtfApeContract.mint(owner.address, 2);
+        // wtfApeContract 合约授权给 nftswap 合约
+        await wtfApeContract.approve(await nFTSwapContract.address, 2);
+        await nFTSwapContract.list(wtfApeContract.address, 2, ethers.utils.parseEther("1"));
+        // console.log("更新前的价格: ", await nFTSwapContract.nftList(wtfApeContract.address, 2));
+        await nFTSwapContract.update(wtfApeContract.address, 2, ethers.utils.parseEther("1.5"));
+        // console.log("更新后的价格: ", await nFTSwapContract.nftList(wtfApeContract.address, 2));
+        const orderNft = await nFTSwapContract.nftList(wtfApeContract.address, 2);
+        expect(orderNft.price).to.equal(BigNumber.from(ethers.utils.parseEther("1.5")));
+    })
+
+    it("下架nft", async() => {
+        // wtfApeContract 合约mint
+        await wtfApeContract.mint(owner.address, 2);
+        // wtfApeContract 合约授权给 nftswap 合约
+        await wtfApeContract.approve(await nFTSwapContract.address, 2);
+        await nFTSwapContract.list(wtfApeContract.address, 2, ethers.utils.parseEther("1.3"));
+        console.log("下架前的价格: ", await nFTSwapContract.nftList(wtfApeContract.address, 2));
+        await nFTSwapContract.revoke(wtfApeContract.address, 2);
+        console.log("下架后的价格: ", await nFTSwapContract.nftList(wtfApeContract.address, 2));
+    })
+
+    it("购买nft", async() => {
+        // wtfApeContract 合约mint
+        await wtfApeContract.mint(owner.address, 2);
+        // wtfApeContract 合约授权给 nftswap 合约
+        await wtfApeContract.approve(await nFTSwapContract.address, 2);
+        await nFTSwapContract.list(wtfApeContract.address, 2, ethers.utils.parseEther("1.3"));
+        console.log("购买前的价格: ", await nFTSwapContract.nftList(wtfApeContract.address, 2));
+        // hardhat  切换不同账户
+        await nFTSwapContract.connect(address1).purchase(wtfApeContract.address, 2, {value: ethers.utils.parseEther("2")});
+        // 0x70997970C51812dc3A010C7d01b50e0d17dc79C8
+        console.log(await wtfApeContract.ownerOf(2), "tokenId为2的NFT的owner");
+        // console.log("购买后的价格: ", await nFTSwapContract.nftList(wtfApeContract.address, 2));
+    })
+    
 
 })
